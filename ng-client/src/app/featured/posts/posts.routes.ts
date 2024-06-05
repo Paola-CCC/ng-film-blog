34458af6c73cca356a5ef3e5d60010e0f8aa7f44
Paola_C <paola.cyprien@hetic.net>
2024-06-05 22:03:28 +0200

    correction path post/id

--- ng-client/src/app/featured/posts/posts.routes.ts
+++ ng-client/src/app/featured/posts/posts.routes.ts
@@ -2,6 +2,6 @@ import { Route, Routes } from "@angular/router";
 import { DetailPostComponent } from "./detail-post/detail-post.component";
 
 export const POSTS_ROUTES: Routes = [
-    { path: 'post/:id', component: DetailPostComponent
+    { path: ':id', component: DetailPostComponent
 },
 ]
