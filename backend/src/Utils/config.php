d885e96dedd6dca9f65eee6a9cf3d09a2a898f80
Paola_C <paola.cyprien@hetic.net>
2024-06-10 00:08:41 +0200

    ng seulement

--- backend/src/Utils/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "u574090949_blog_films");
-define("DB_CHARSET", "utf8");
-define("DB_USER", "u574090949_root");
-define("DB_PASSWORD", "Pass_root9091+");
-define("JWT_SECRET", "UnPetitSecretBienGarde");
